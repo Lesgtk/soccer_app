389e1600cfc2fbd72f5e047c49e6b3ba156d8e2b
lesgtk <lesgtk1510@gmail.com>
2020-07-24 17:15:41 +0900

    フォームを送信

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  # 下記以外を許可しない
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end

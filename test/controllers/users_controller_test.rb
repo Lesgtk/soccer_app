01836c7726d7b2ef15637966fd2400caf519d8b9
lesgtk <lesgtk1510@gmail.com>
2020-07-25 23:28:19 +0900

    認可機能追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -56,4 +56,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @other_user.reload.admin?
   end
 
+  # 管理者の場合
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+    delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  # 管理者出ない場合
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
 end

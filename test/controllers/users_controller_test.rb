ece9aa70210948c4018d1843e6245ed6eb470055
lesgtk <lesgtk1510@gmail.com>
2020-07-25 21:55:25 +0900

    編集、更新ページの保護

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   # サインアップリングに成功するかどうか
@@ -25,6 +26,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                           email: @user.email }}
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  # ユーザーが別ユーザーを編集できるか確認
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  # ユーザーが別ユーザーをupdateできるか確認
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
 end

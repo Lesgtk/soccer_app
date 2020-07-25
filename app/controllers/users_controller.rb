004062c9ba90d6e6d42109c388589bb3e545b34b
lesgtk <lesgtk1510@gmail.com>
2020-07-25 21:02:25 +0900

    編集失敗機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,14 @@ class UsersController < ApplicationController
     @user = User.find_by(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
+
 private
 
   # 下記以外を許可しない

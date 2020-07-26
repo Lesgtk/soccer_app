786273178bfa271a149ce265bd501d61b3117022
lesgtk <lesgtk1510@gmail.com>
2020-07-26 22:28:09 +0900

    postのバリデーション

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+
+  def setup
+     @user = users(:michael)
+     # このコードは慣習的に正しくない
+     @post = Post.new(content: "テスト", user_id: @user.id)
+   end
+
+   # ポストの確認
+   test "should be valid" do
+     assert @post.valid?
+   end
+
+   # user_idが存在するかどうか
+   test "user id should be present" do
+     @post.user_id = nil
+     assert_not @post.valid?
+   end
+ end

d34417083a4fc1a34d38184283b0a480db11ead0
lesgtk <lesgtk1510@gmail.com>
2020-07-26 16:36:33 +0900

    有効化のテスト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "メールを確認して、アカウントを有効にしてください"
       redirect_to root_url
     else

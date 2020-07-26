8e1170977968842f9dc6e9022c85c2c68c7e2330
lesgtk <lesgtk1510@gmail.com>
2020-07-26 16:03:42 +0900

    editアクションで有効化

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "アカウントは有効化しました"
+      redirect_to user
+    else
+      flash[:danger] = "有効化リンクは無効です"
+      redirect_to root_url
+    end
+  end
 end

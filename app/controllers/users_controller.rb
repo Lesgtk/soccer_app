3ac2ca064ffd3b41230cad63bc35581bbf356c05
lesgtk <lesgtk1510@gmail.com>
2020-07-26 15:14:59 +0900

    createアクション更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "ようこそ SOOCER APPへ!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "メールを確認して、アカウントを有効にしてください"
+      redirect_to root_url
     else
       render 'new'
     end

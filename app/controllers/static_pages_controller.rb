297eea3b11dd81bc8bc918bea6517e36666f97a3
lesgtk <lesgtk1510@gmail.com>
2020-07-28 22:00:09 +0900

    homeの投稿一覧追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
        @post  = current_user.posts.build
-       @feed_items = current_user.feed
+       @feed_items = Post.all
      end
   end
 

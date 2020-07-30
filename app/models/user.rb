01f5b85240ebe6af007cde146c6d67ac850f9bd0
lesgtk <lesgtk1510@gmail.com>
2020-07-30 23:06:58 +0900

    セブセレクト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,10 @@ class User < ApplicationRecord
 
   # 試作feedの定義
   def feed
-    Post.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする

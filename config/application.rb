5e6782eb225280cf830aac688a31efaba7138af2
lesgtk <lesgtk1510@gmail.com>
2020-09-06 22:19:48 +0900

    テスト導入

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,13 @@ module SoccerApp
 
     # kaminari
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false
+    end
+
   end
 end

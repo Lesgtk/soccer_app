795a4ab03c93a8f16f60afb5c475d5657363a0a8
lesgtk <lesgtk1510@gmail.com>
2020-09-26 11:46:53 +0900

    メール送信機能修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:"smtp.gmail.com",
+      domain: 'gmail.com',
+      port:587,
+      user_name: Rails.application.credentials.gmail[:user_name],
+      password: Rails.application.credentials.gmail[:password],
+      authentication: :login
+    }
 
   config.action_mailer.perform_caching = false
 
